ad494299b1587b80b5aaddaddb708d4cdf5241cd
owo404 <xian101221@gmail.com>
2025-04-11 20:40:04 +0900

    add new bone

---
+++ compiler/coric/src/main.rs
@@ -0,0 +1,41 @@
+use std::env;
+use std::fs;
+use std::process::Command;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("wrong!");
+        println!("coric <main.dr>");
+        return;
+    }
+
+    let contents = match fs::read_to_string(&args[1]) {
+        Ok(f) => f,
+        Err(_) => {
+            println!("can't read file");
+            println!("coric <main.dr>");
+            return;
+        }
+    };
+
+    let tokens = coric_lexer::tokenize(contents);
+
+    let prog = parser::parse_prog(&tokens);
+    if prog.is_none() {
+        println!("invalid program!");
+        return;
+    }
+
+    let output = generation::gen_prog(prog.unwrap());
+
+    fs::write("out.asm", output).unwrap();
+    Command::new("nasm")
+        .args(["-felf64", "out.asm"])
+        .spawn()
+        .unwrap();
+    Command::new("ld")
+        .args(["out.o", "-o", "out"])
+        .spawn()
+        .unwrap();
+}
