a2699fadd364886e97f7b77f3a6dbd60a63d519a
enpotid <xian101221@gmail.com>
2025-04-26 23:41:22 +0900

    update ast

--- compiler/coric_ast/src/ast.rs
+++ compiler/coric_ast/src/ast.rs
@@ -13,10 +13,48 @@ pub struct Item {
 }
 
 #[derive(Debug, Clone)]
+pub struct Ident {
+    pub ident: String,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone)]
+pub struct Ty {
+    // todo
+    pub span: Span,
+}
+
+#[derive(Debug, Clone)]
 pub enum ItemKind {
-    Nec,
+    Nec(Ident),
     Index,
-    Enum,
-    Struct,
+    Enum(Ident, EnumDef),
+    Struct(Ident, VariantData),
     Fn,
+}
+
+#[derive(Debug, Clone)]
+pub struct EnumDef {
+    pub variants: Vec<Variant>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Variant {
+    pub ident: Ident,
+    pub data: VariantData,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone)]
+pub enum VariantData {
+    Struct(Vec<FieldDef>),
+    Tuple(Vec<FieldDef>),
+    Unit,
+}
+
+#[derive(Debug, Clone)]
+pub struct FieldDef {
+    pub ident: Option<Ident>,
+    pub ty: Ty,
+    pub span: Span,
 }
